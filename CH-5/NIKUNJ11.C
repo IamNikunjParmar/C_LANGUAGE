a6b4bc9b72df6deb3295b7e84fcb4244d0d2fd70
NIKUNJ PARMAR <121547318+nikunjparmar21899@users.noreply.github.com>
2022-12-29 02:55:33 -0800

    Add files via upload

---
+++ CH-5/NIKUNJ11.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    int x,y,sum;
+    clrscr();
+    printf("Enter the value of x :- ");
+    scanf("%d", &x);
+    printf("Enter the value of y :- ");
+    scanf("%d", &y);
+    sum =(x*x)+(2*x*y)+(y*y);
+    printf("sum is = %d", sum);
+
+
+
+
+    getch();
+
+
+}
---
+++ CH-5/NIKUNJ14.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    int x,y,z,sum;
+    clrscr();
+    printf("Enter the value of x :- ");
+    scanf("%d", &x);
+    printf("Enter the value of y :- ");
+    scanf("%d", &y);
+
+    printf("Enter the value of z :- ");
+    scanf("%d", &z);
+
+    sum =(x*x*x)-(3*x*x*y)+(3*x*y*y)-(y*y*y);
+
+    printf("sum is = %d", sum);
+
+
+
+
+    getch();
+
+
+}
