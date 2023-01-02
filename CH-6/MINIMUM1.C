66925e44d87bfb8ea38129d0dc52c1c1c1f552b0
NIKUNJ PARMAR <121547318+nikunjparmar21899@users.noreply.github.com>
2023-01-02 02:31:49 -0800

    Add files via upload

---
+++ CH-6/MINIMUM1.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+       int a,b;
+       clrscr();
+
+       printf("enter the number of a :- ");
+       scanf("%d",&a);
+       printf("enter the number of b :- ");
+       scanf("%d",&b);
+
+       if(a>b)
+      {
+      printf("b is minimum \n");
+
+      }
+      else
+      {
+      printf("a is minimum \n");
+
+      }
+
+       getch();
+
+
+
+}
